83095034af925dbcd11f213d7b52eff8d1c747ef
Kadyrmatov <janarbek.bekmyrzauulu.alt@gmail.com>
2022-06-06 07:57:47 +0600

    add

---
+++ main.js
@@ -0,0 +1,96 @@
+// let a = prompt("Сан жазыныз");
+// let myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (let i = 0; i < myNumber.length; i++) {
+//   if (myNumber[i] > 5 && myNumber[i] < 12) {
+//     console.log(myNumber[i]);
+//   }
+// }
+// Периметрин эсептеген функция
+// function myNumber(){
+//     let a = prompt("a jagyn jazynyz");
+//     a = parseInt(a);
+//     return a;
+// }
+
+// function myNumber1(){
+//     let b = prompt("b jagyn jazynyz");
+//     b = parseInt(b);
+//     return b;
+// }
+
+// let result = (myNumber() + myNumber1()) * 2;
+// document.write(result);
+
+//Саатты  боюнча жакшы соз айтта турган
+// let tami = prompt("Саат канча болду?");
+
+// if(tami >= 6 && tami <= 10){
+//     document.write('Кутман тан!');
+// }else if(tami >= 10 && tami <= 18){
+//     document.write('Кутман кун!');
+// }else if(tami >= 18 && tami <= 22){
+//     document.write('Кутман кеч!');
+// }else {
+//     document.write('Кутман тун!');
+// }
+
+// тапшырма аткарылды jeobyc туку 2 задания
+// let user = {
+//     name: 'Janar',
+//     age: 20,
+//     email: "zhanar@gmail.com",
+//     passport: "janar2002"
+// }
+
+// let sum = 5;
+// if (user.email != prompt("email лынызды жазыныз")) {
+//   alert("кечиресин, мындай почта жок");
+// } else if (user.passport != prompt("Кодунузду жазыныз")) {
+//   alert("кодунуз туура эмес");
+// } else if (sum == prompt("2+3=? туура эсептесен, премия аласын")) {
+//   user.sum = 5;
+//   alert(`Туура! сенин премиян = ${user.sum}`);
+// } else {
+//   alert("туура эмес жооп бердин");
+// }
+
+// for(let i = 0; i <= 100; i++)(
+//     console.log(i)
+// )
+
+// for(let i = 100; i > 0; i--)(
+//     console.log(i)
+// )
+
+// for(let i = 2; i < 10; i++){
+//     if(i % 2 == 0){
+//         alert(i)
+//     }
+// }
+
+// let myNumber = [];
+// for(let i = 0; i <= 10; i++){
+//     myNumber.push(i, "x")
+//     console.log(myNumber)
+// }
+
+// let arr = [-2, 1, 4, -1, -3, 8, 7, 0, -5, 18, 3, 12];
+
+// for(let i = -5; i < arr.length; i++){
+//     if(arr[i] >= 1 && arr[i] <= 10)
+//     console.log(arr[i])
+// }
+
+// let arr = [-2, 1, 4, -1, -3, 8, 7, 0, -5, 18, 3, 12];
+
+// for(let i = -5; i < arr.length; i++){
+//     if(arr[i] == -5)
+//     console.log(arr[i])
+// }
+
+// let arr = [1, 2, 7, 8, 3, 9, 4, 5];
+
+// for(let i = 0; i < arr.length; i++){
+
+// }
